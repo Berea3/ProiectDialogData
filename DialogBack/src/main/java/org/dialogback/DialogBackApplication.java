8932a1c6b526854d3fad874250eb94eb020d2f24
Berendea Andrei <berendeaandrei03@gmail.com>
2024-09-24 18:52:18 +0300

    created project

---
+++ DialogBack/src/main/java/org/dialogback/DialogBackApplication.java
@@ -0,0 +1,13 @@
+package org.dialogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DialogBackApplication.class, args);
+    }
+
+}
---
+++ DialogBack/src/test/java/org/dialogback/DialogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dialogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
