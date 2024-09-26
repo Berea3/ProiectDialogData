f911bdb9d7130cd0529e6b1a82f6ab511c3658db
Berendea Andrei <berendeaandrei03@gmail.com>
2024-09-26 19:25:37 +0300

    material, compiler

---
+++ DialogFront/src/app/entities/Product.ts
@@ -0,0 +1,18 @@
+export class Product{
+    id: string;
+
+    brand: string;
+    model: string;
+
+    cpu: string;  // detail
+    gpu: string;  // detail
+    ram: string;  // detail
+
+    price: number;
+
+    details: string;  // detail
+
+    inStock: boolean;
+
+    weight: number;
+}
