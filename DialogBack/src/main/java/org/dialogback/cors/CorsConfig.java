c51c6d5aa2e96adb6fbb911c67480460679aa268
Berendea Andrei <berendeaandrei03@gmail.com>
2024-09-29 12:48:42 +0300

    cors

---
+++ DialogBack/src/main/java/org/dialogback/cors/CorsConfig.java
@@ -0,0 +1,20 @@
+package org.dialogback.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4200") // Replace with your Angular app domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedOriginPatterns("http://localhost:4200")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
